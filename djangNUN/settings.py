10207907c278689bc1d533282af357fa473dc4f4
Himadri Singh <himadrisingh11@gmail.com>
2023-05-04 00:51:23 +0530

    gunicorn

--- djangNUN/settings.py
+++ djangNUN/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-)@z++a9oijfkm20y(&^kd1d)ac@#!m)lk-^&^et)$1j#i+m@u1
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app']
+ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app','*']
 
 
 # Application definition
